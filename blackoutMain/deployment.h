c462bcbeeeddc44df4dcf0ed542836d0769bcdf0
a-duen <anthony.duen@gmail.com>
2019-06-18 19:50:15 -0600

    MORE MACROS

--- blackoutMain/deployment.h
+++ blackoutMain/deployment.h
@@ -11,9 +11,21 @@
 #define PWM_DELAY 15
 #define PWM_STRENGTH 10 //out of 255
 
-
 #define DEPLOY_CHANNEL 0
 #define RELEASE_CHANNEL 1
+
+#define LOW_PID_OUT -3000 //-40000  //Throttle
+#define HIGH_PID_OUT 3000 //40000   //Throttle
+#define LOW_PID_OUT_P -1000         //Pitch
+#define HIGH_PID_OUT_P 1000         //Pitch
+#define LOW_PID_OUT_R -1000         //Roll
+#define HIGH_PID_OUT_R 1000         //Roll
+#define LOW_PID_OUT_Y -30000         //Yaw
+#define HIGH_PID_OUT_Y 30000         //Yaw
+
+#define COUNT_LOW 3222     //999 = (0%)
+#define COUNT_MID 4870    //1500 = (50%)
+#define COUNT_HIGH 6520   //2001 = (100%)
 
 void initDeployment();
 void deployChute();
